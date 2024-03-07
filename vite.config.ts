3b42fd43de02aa6025ac6d947fc143a10f1f1098
MIMBOL228 <aekemerov@yandex.ru>
2024-03-07 11:14:53 +0500

    Set base to null

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/streamfony_frontend/'
+  plugins: [react()]
 })
